fc2e778e00fc5d2f2ad86148bdd4b928544375a1
Wei Cao <viviancao0728@gmail.com>
2021-11-18 15:48:04 +0100

    ok

--- People_MVC/Controllers/HomeController.cs
+++ People_MVC/Controllers/HomeController.cs
@@ -33,5 +33,14 @@ namespace People_MVC.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
     }
 }
